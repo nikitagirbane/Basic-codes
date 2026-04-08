86ebe2f86fdb44c9e3295ed569a07cc2a36ce0eb
Nikita Girbane <nikitagirbane1205@gmail.com>
2026-04-08 21:06:10 +0530

    Add files via upload

---
+++ leapyear.py
@@ -0,0 +1,12 @@
+def leapyear(year):
+    leap = True
+    if(year%4==0):
+        return True
+    if(year%100!=0):
+        return False
+    if(year%400==0):
+        return True
+    return leap
+
+year = int(input("Enter Year :"))
+print(leapyear(year))
